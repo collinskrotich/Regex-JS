a46c3bb1791e8c4ca01b8c03767853a25928a6fa
Collins Kiprono Rotich <kolinsr@gmail.com>
2022-10-15 10:00:15 +0300

    Regex match beginning string patterns

--- consecutivecharacters.js
+++ consecutivecharacters.js
@@ -27,7 +27,9 @@ gPhrase.match(goRegex);
 oPhrase.match(goRegex);
 // In order, the three match calls would return the values ["goooooooo"], ["g"], and null.
 
-// For this challenge, chewieQuote has been initialized as the string Aaaaaaaaaaaaaaaarrrgh! behind the scenes. Create a regex chewieRegex that uses the * character to match an uppercase A character immediately followed by zero or more lowercase a characters in chewieQuote. Your regex does not need flags or character classes, and it should not match any of the other quotes.
+// For this challenge, chewieQuote has been initialized as the string Aaaaaaaaaaaaaaaarrrgh! behind the scenes. 
+// Create a regex chewieRegex that uses the * character to match an uppercase A character immediately followed by zero or more lowercase a characters in chewieQ
+// Quote. Your regex does not need flags or character classes, and it should not match any of the other quotes.
 
 // Only change code below this line
 let chewieRegex = /Aa*/; // Change this line
